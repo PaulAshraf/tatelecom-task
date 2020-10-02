68960ca7fb7af4ad5d2036b9e590ed4e9915bf41
PaulAshraf <paulashraf14@gmail.com>
2020-10-02 08:49:21 +0200

    redux fetch working

--- src/features/cards/CardsList.js
+++ src/features/cards/CardsList.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import data from './data.json'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchCards, selectAllCards } from './cardSlice'
+import { Spinner, Alert } from 'react-bootstrap'
 import './card.scss'
 import Card from './Card'
 
 const CardsList = () => {
+
+    const dispatch = useDispatch()
+    
+    const cards = useSelector(selectAllCards)
+    const cardStatus = useSelector((state) => state.cards.status)
+    const error = useSelector((state) => state.cards.error)
+
+    useEffect(() => {
+        console.log(cardStatus)
+        if (cardStatus === 'idle') {
+            dispatch(fetchCards())
+          }
+    }, [cardStatus, dispatch])
+
     return (
         <div>
             <h1>الجمعيات الخيرية</h1>
             <div className='wrapper'>
-                {data.data.map(card => <Card title={card.Name} imgUrl={card.Logo} />)}
+                {cardStatus === 'loading' ? 
+                    <Spinner /> : cardStatus === 'error' ? <Alert variant='danger' >{error}</Alert> :
+                        cards.map(card => <Card key={card.pkID} title={card.Name} imgUrl={card.Logo} />)}
             </div>
         </div>
     )
---
+++ src/features/cards/cardSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+const initialState = {
+  cards: [],
+  status: 'idle',
+  error: null,
+}
+
+export const fetchCards = createAsyncThunk('cards/fetchCards', async () => {
+  const response = await axios.get('https://megakheir.herokuapp.com/ngos')
+  console.log(response.data.data)
+  return response.data.data
+})
+
+const cardsSlice = createSlice({
+  name: 'cards',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchCards.pending]: (state, _) => {
+      state.status = 'loading'
+    },
+    [fetchCards.fulfilled]: (state, action) => {
+      state.status = 'succeeded'
+      // Add any fetched Cards to the array
+      state.cards = state.cards.concat(action.payload)
+    },
+    [fetchCards.rejected]: (state, action) => {
+      state.status = 'failed'
+      state.error = action.payload
+    },
+  },
+})
+
+export default cardsSlice.reducer
+
+export const selectAllCards = (state) => state.cards.cards
